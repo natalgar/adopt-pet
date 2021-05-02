1adf5f7fa22081eaeb449a432a9476e691d55389
natalgar <natalie.garcia@microsoft.com>
2021-05-02 12:10:18 -0700

    use hooks and implement custom hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import * as ReactDOM from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Mundle" animal="Dog" breed="Corgi" />
-      <Pet name="Macy" animal="Cat" breed="Tabby" />
-      <Pet name="Phoenix" animal="Bird" breed="Cockatiel" />
+      <SearchParams />
     </div>
   );
 };
